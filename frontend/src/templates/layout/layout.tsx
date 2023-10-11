5e781367f9b1ab02b372c364776bf4821031526e
Tiago Carvalho <tiago.carvalho@grupomult.com.br>
2023-10-11 16:56:56 -0300

    feat(navbar) more adjustments

--- frontend/src/templates/layout/layout.tsx
+++ frontend/src/templates/layout/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styles.scss";
 import selectedImg from "./images/SciFi_Circle01Cyan_02.png";
+import spin from "./images/Icon_MenuIcon08_Spin.png";
+import shop from "./images/shop.png";
 import { GiSwitchWeapon, GiBattleGear, GiCrossedSwords } from "react-icons/gi";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
 import { BiDownload } from "react-icons/bi";
@@ -8,23 +10,23 @@ import { BiDownload } from "react-icons/bi";
 export const LayoutPage = () => {
   const options = [
     {
-      option: <GiCrossedSwords color="#29777b" size={"2rem"} />,
+      option: <GiCrossedSwords color="#4e809f" size={"2rem"} />,
       id: "1",
     },
     {
-      option: <GiBattleGear color="#29777b" size={"2rem"} />,
+      option: <GiBattleGear color="#4e809f" size={"2rem"} />,
       id: "2",
     },
     {
-      option: <PiShoppingCartSimpleFill color="#29777b" size={"2rem"} />,
+      option: <img src={shop} alt=""/>,
       id: "3",
     },
     {
-      option: <GiSwitchWeapon color="#29777b" size={"2rem"} />,
+      option: <GiSwitchWeapon color="#4e809f" size={"2rem"} />,
       id: "4",
     },
     {
-      option: "Change class",
+      option: <img src={spin} alt=""/>,
       id: "5",
     },
     {
@@ -32,7 +34,7 @@ export const LayoutPage = () => {
       id: "6",
     },
     {
-      option: <BiDownload color="#29777b" size={"2rem"} />,
+      option: <BiDownload color="#4e809f" size={"2rem"} />,
       id: "7",
     },
   ];
