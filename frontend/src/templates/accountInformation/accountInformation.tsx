c08cccec0ac911d5e1bf169736aec939b22a42c7
Tiago Carvalho <tiago.carvalho@grupomult.com.br>
2023-10-12 03:22:59 -0300

    feat(account) beggining account info code

--- frontend/src/templates/accountInformation/accountInformation.tsx
+++ frontend/src/templates/accountInformation/accountInformation.tsx
@@ -8,13 +8,13 @@ export const AccountInformation = () => {
         <div className="account-login-information-text">
           <span>User: Tiagokill</span>
           <span>Last login: Thu, 05 Oct 2023 12:43 PM</span>
-          <span className="kave-line">IP Address:</span>
-          <span className="kave-line">Bank password:</span>
-          <span className="kave-line">Pin:</span>
-          <span className="kave-line">Rank Cp race:</span>
-          <span className="kave-line">Rank Cp world:</span>
-          <span className="kave-line">Rank Achievements:</span>
-          <span className="kave-line">Rank Battle pass:</span>
+          <span>IP Address:</span>
+          <span>Bank password:</span>
+          <span>Pin:</span>
+          <span>Rank Cp race:</span>
+          <span>Rank Cp world:</span>
+          <span>Rank Achievements:</span>
+          <span>Rank Battle pass:</span>
         </div>
       </div>
     </div>
